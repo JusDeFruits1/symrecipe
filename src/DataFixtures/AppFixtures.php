b5a84084d951aecc91742e769762715bc2274245
Chanfi Mari Yram <152385957+JusDeFruits1@users.noreply.github.com>
2025-05-22 10:34:56 +0200

    Partie 3: CRUD des recettes - Entité Recipe

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Ingredient;
+use App\Entity\Recipe;
 use Faker\Factory;
 use Faker\Generator;
 
@@ -22,7 +23,18 @@ class AppFixtures extends Fixture
             $ingredient->setName($this->faker->word())
                 ->setPrice(mt_rand(1, 199));
             $manager->persist($ingredient);
-        }        
+        }
+        for ($i = 1; $i <= 25; $i++) {
+            $recipe = new Recipe();
+            $recipe->setName($this->faker->words(3, true))
+                   ->setTime(mt_rand(5, 120))
+                   ->setNbPersons(mt_rand(1, 10))
+                   ->setDifficulty(mt_rand(1, 5))
+                   ->setDescription($this->faker->text(300))
+                   ->setPrice(mt_rand(0, 200))
+                   ->setIsFavorite($this->faker->boolean());
+            $manager->persist($recipe);
+        }
         $manager->flush();
     }
 }
